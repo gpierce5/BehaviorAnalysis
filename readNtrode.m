ff58b9bc9e701054f9f250e532315fd8b1ff1d36
gpierce5 <pierce.gm@gmail.com>
2016-09-26 12:53:40 -0400

    changed readNtrode in small way

--- readNtrode.m
+++ readNtrode.m
@@ -1,14 +1,12 @@
 function [ traces ] = readNtrode(filepath,ntrodefile, signal)
 %readNtrode IN PROGRESS
-%   readNtrode(filepath) will read all ntrode files located in a folder
-%   of a single session. It will look for signal names included in the file
-%   behavior_signals.mat
+%   readNtrode(filepath, ntrodefile) will read the ntrode files from that session. 
+%   It will look for signal names included in the file
+%   behavior_signals.mat.
 %
 %   readNtrode(filepath, signal) will read only the particular signal.
 %   Signal should be a cell array of with each cell containing a string of
 %   the signal(s) you want to read
-%   added a mandatory second term, ntrodefile, so program will work if
-%   files are not named the same as the typical
 
 switch nargin
     case 2
